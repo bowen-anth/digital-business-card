d8fc96abeee3c6017cf3aaf570255a8b164a0fe2
bowen-anth <bowen.anth@gmail.com>
2023-12-22 16:16:49 -0700

    added Photo and Name components

---
+++ src/components/Name.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Name = () => {
+    return (
+        <>
+        <div className="name-container">
+            <span className="name-container-name"><name>Tony Bowen</name></span>
+            <span className="name-container-role">React Developer</span>
+            <a href="mailto: bowen.anth@gmail.com">bowen.anth@gmail.com</a>
+        </div>
+        </>
+    )
+}
+
+export default Name
---
+++ src/components/Photo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Photo = () => {
+    return (
+        <>
+        <div className="profile-photo-container">
+            <img className="profile-photo" src="me.png" alt="a profile picture of Tony" width="400px" height="auto" />
+        </div>
+        </>
+    )
+}
+
+export default Photo
